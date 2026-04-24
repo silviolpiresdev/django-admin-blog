1b6a5e74f55b99b59cd8f798771efec429d5d985
silviolpires <silviolpires.dev@gmail.com>
2026-04-24 09:27:52 -0300

    feat: personaliza admin com inlines, PostAdmin e editor markdown

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,29 @@
+from django.db import models
+from mdeditor.widgets import MDEditorWidget
 from django.contrib import admin
 from .models import Post, Tecnologia
+from django.contrib.auth.models import User
+
+admin.site.unregister(User)
+
+class PostInLine(admin.StackedInline):
+    model = Post
+    
+    
+class UserAdmin(admin.ModelAdmin):
+    inlines = [
+        PostInLine, 
+               ]
 
 
-
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': MDEditorWidget}
+    }
+    
+    
+    
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tecnologia)
 # Register your models here.
--- setup/settings.py
+++ setup/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'mdeditor',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
