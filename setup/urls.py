42d527cca28f530dd69448481ee004a91b85ff59
silviolpires <silviolpires.dev@gmail.com>
2026-04-24 11:50:03 -0300

    feat: instala e configura django-jet-reboot como tema do admin

--- setup/urls.py
+++ setup/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mdeditor/', include('mdeditor.urls')),
+    path('jet/', include('jet.urls', 'jet')),
 ]
